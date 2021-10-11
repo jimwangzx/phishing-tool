8cb8eb889fb896ce74b9c07a1d735d1e77ac8c03
Sentinion <stuart@sryan.biz>
2021-10-11 13:34:53 -0600

    Turned code into functions
    Also started on the email model creation but not much written there now as I'm looking into the python email library

--- model_creation.py
+++ model_creation.py
@@ -1,35 +1,31 @@
 # Written by Stuart Ryan, October 2021
+
 # Python packages for Natrual Language Processing
 import re
 from nltk.stem.porter import PorterStemmer
-import nltk
 from nltk.corpus import stopwords
-# Python package for TensorFlow
-#import tensorflow
-# Python package for pandas
+
+# Python package for holding data
 import pandas as pd
-import re
+
+# Sklearn packages for model creation
 from sklearn.pipeline import Pipeline
-from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import GridSearchCV
-from sklearn.model_selection import cross_val_score
 from sklearn.linear_model import SGDClassifier
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.model_selection import train_test_split
 
+# Python package for parsing data from email files
+import email
 
 '''
 Email data gotten from: https://www.kaggle.com/veleon/ham-and-spam-dataset
-Text data gotten from: https://www.kaggle.com/uciml/sms-spam-collection-dataset
+Text data gotten from: https://www.kaggle.com/uciml/sms-spam-collection-dataset5
 '''
+BAD_SYMBOLS_RE = re.compile('[^0-9a-z #+_]')
+STOPWORDS = set(stopwords.words('english'))
 
-### Text Data Collecting and Preprocessing ###
-texts_df = pd.read_csv(r"text_data\spam.csv", encoding="ISO-8859-1")
-print(texts_df)
-texts_df.dropna(1, inplace=True) #Removing excess variable columns
-print(texts_df)
-
-def preprocessor(text): # Getting rid of emojis as they are commonplace in text messages 
+def clean_text(text):
     emojis = re.compile(
     "(["
     "\U0001F1E0-\U0001F1FF"  # flags (iOS)
@@ -46,27 +42,15 @@ def preprocessor(text): # Getting rid of emojis as they are commonplace in text 
     "])")
 
     emoticons = re.findall('(?::|;|=)(?:-)?(?:\)|\(|D|P)',
-                           text)
-    text = (re.sub('[\W]+', ' ', text.lower()) +
-            ' '.join(emoticons).replace('-', ''))
-    text = re.sub(emojis, r' \1 ', text)
-    return text
+                        text)
 
-BAD_SYMBOLS_RE = re.compile('[^0-9a-z #+_]')
-STOPWORDS = set(stopwords.words('english'))
-
-def clean_text(text):
     text = text.lower() # lowercase text
     text = BAD_SYMBOLS_RE.sub('', text) # delete symbols which are in BAD_SYMBOLS_RE from text
     text = ' '.join(word for word in text.split() if word not in STOPWORDS) # delete stopwors from text
+    text = (re.sub('[\W]+', ' ', text.lower()) +
+            ' '.join(emoticons).replace('-', '')) # getting rid of emoticons
+    text = re.sub(emojis, r' \1 ', text) # getting rid of emojis
     return text
-
-texts_df['v2'] = texts_df['v2'].apply(clean_text)
-texts_df['v2'] = texts_df['v2'].apply(preprocessor)
-print(texts_df)
-
-### Text Data Model Creation ###
-stop = stopwords.words('english')
 
 porter = PorterStemmer()
 def tokenizer(text):
@@ -74,57 +58,73 @@ def tokenizer(text):
 
 def tokenizer_porter(text):
     return [porter.stem(word) for word in text.split()]
+        
+def text_model_creation():
+    ### Text Data Collecting and Preprocessing ###
+    texts_df = pd.read_csv(r"text_data\spam.csv", encoding="ISO-8859-1")
+    texts_df.dropna(1, inplace=True) #Removing excess variable columns
+    texts_df['v2'] = texts_df['v2'].apply(clean_text)
+    print(texts_df)
 
+    ### Text Data Model Creation ###
+    # 70% of the data allocated to training
+    X = texts_df.v2
+    y = texts_df.v1
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state = 1)
 
-# 70% of the data allocated to training
-X = texts_df.v2
-y = texts_df.v1
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state = 1)
+    tfidf = TfidfVectorizer(strip_accents=None,
+                            lowercase=False,
+                            preprocessor=None)
 
-tfidf = TfidfVectorizer(strip_accents=None,
-                        lowercase=False,
-                        preprocessor=None)
+    # Look to expand the paramater grid to include as many relevant variables as possible
+    '''
+    Parameter choices:
+    TfIdfVectorizer:
+        - ngram_range: Chose to consider 1. unigrams and 2. unigrams with bigrams for text messages as texts are short and often times contain few keywords
+        - tokenizer: Testing to see if stemming would be beneficial given the informal nature of words in texts (slang words)
+    SGDClassifier:
+        - clf_penalty: Chose to use both l1 and l2 because not sure whether the built in feature selection of l2 will be more important than l1's ability to ignore outliers in our dataset
+        - clf_loss: Comparing log to hinge to compare if SVM is better for classification than logisitc regression
+    '''
+    param_grid = [{'vect__ngram_range': [(1,1), (1, 2), (2,2)],
+                'vect__tokenizer': [tokenizer, tokenizer_porter],
+                'vect__use_idf':[False, True],
+                'clf__penalty': ['l1', 'l2'],
+                'clf__alpha': [.00001, .0001, .001, .01],
+                'clf__loss': ['log', 'hinge']
+                }
+                ]
 
-# Look to expand the paramater grid to include as many relevant variables as possible
-'''
-Parameter choices:
-TfIdfVectorizer:
-    - ngram_range: Chose to consider 1. unigrams and 2. unigrams with bigrams for text messages as texts are short and often times contain few keywords
-    - stop_words: Chose to consider with stop words and without because not sure if removal will help with classification due to the informal nature of texts
-    - norm: Using l2 (default) as texts can vary in length greatly
-SGDClassifier:
-    - clf_penalty: Chose to use both l1 and l2 because not sure whether the built in feature selection of l2 will be more important 
-    than l1's ability to ignore outliers in our dataset. Also chose to include elastic net as it mitigates the negatives of both l1 and l2
-    - l1_ratio: Defaults to .15 when elasticnet is used so testing an even split of l1 and l2 as well as leaning towards l1 regularization
-'''
-param_grid = [{'vect__ngram_range': [(1,1), (1, 2), (2,2)],
-               'vect__tokenizer': [tokenizer, tokenizer_porter],
-               'vect__use_idf':[False, True],
-               'clf__penalty': ['l1', 'l2'],
-               'clf__alpha': [.00001, .0001, .001, .01],
-               'clf__loss': ['log', 'hinge']
-               }
-              ]
+    lr_tfidf = Pipeline([('vect', tfidf),
+                        ('clf', SGDClassifier(loss='log', random_state=1, max_iter=5, tol=None))])
 
-lr_tfidf = Pipeline([('vect', tfidf),
-                     ('clf', SGDClassifier(loss='log', random_state=1, max_iter=5, tol=None))])
+    gs_lr_tfidf = GridSearchCV(lr_tfidf, param_grid,
+                            scoring='accuracy',
+                            cv=5,
+                            verbose=2,
+                            n_jobs=-1) # Utilizes all cores on machine to speed up grid search
 
-gs_lr_tfidf = GridSearchCV(lr_tfidf, param_grid,
-                           scoring='accuracy',
-                           cv=5,
-                           verbose=2,
-                           n_jobs=-1) # Utilizes all cores on machine to speed up grid search
+    gs_lr_tfidf.fit(X_train, y_train)
 
-gs_lr_tfidf.fit(X_train, y_train)
+    print('Best parameter set: %s ' % gs_lr_tfidf.best_params_)
+    print('CV Accuracy: %.3f' % gs_lr_tfidf.best_score_)
 
-print('Best parameter set: %s ' % gs_lr_tfidf.best_params_)
-print('CV Accuracy: %.3f' % gs_lr_tfidf.best_score_)
+    bestModel = gs_lr_tfidf.best_estimator_
+    print('Test Accuracy: %.3f' % bestModel.score(X_test, y_test))
 
-bestModel = gs_lr_tfidf.best_estimator_
-print('Test Accuracy: %.3f' % bestModel.score(X_test, y_test))
+    '''
+    The following was the best parameter set found using the above grid search:
+    Best parameter set: {'clf__alpha': 1e-05, 'clf__loss': 'log', 'clf__penalty': 'l2', 'vect__ngram_range': (1, 2), 'vect__tokenizer': <function tokenizer_porter at 0x0000023946B04438>, 'vect__use_idf': True} 
+    CV Accuracy: 0.984
+    Test Accuracy: 0.983
+    '''
+def email_model_creation():
+    ### Email Data Collecting and Preprocessing ###
+    
+    email_df = pd.read_csv(r"text_data\spam.csv", encoding="ISO-8859-1")
+    email_df.dropna(1, inplace=True) #Removing excess variable columns
+    #texts_df['v2'] = texts_df['v2'].apply(clean_text)
+    #print(texts_df)
+    pass
 
-# Will need to pull in some validation data - Will look into this
-
-### Email Data Preprocessing ###
-
-
+email_model_creation()
